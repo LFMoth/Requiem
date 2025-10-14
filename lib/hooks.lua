6f704872f42847aea188c1482adb9e641df60c73
LFMoth <adrielrodriguez.arm@gmail.com>
2025-10-14 17:12:16 +0200

    fixed cat cards

--- lib/hooks.lua
+++ lib/hooks.lua
@@ -5,3 +5,10 @@ function Card:get_id()
     end
     return catcardgetid(self)
 end
+local catcardisface = Card.is_face -- Code to make Card Cat count as a face card
+function Card:is_face(from_boss)
+    if SMODS.has_enhancement(self, 'm_req_cat') then
+        return true
+    end
+    return catcardisface(self)
+end
