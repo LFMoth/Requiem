ef817a8e51e5a22d3d15737971ad77feb776d937
LFMoth <adrielrodriguez.arm@gmail.com>
2025-10-23 12:09:04 +0200

    nerf: stacked cards

--- items/enhancements.lua
+++ items/enhancements.lua
@@ -2,17 +2,6 @@
 -- Card Cat
 SMODS.Enhancement {
     key = 'cat',
-    cry_credits = {
-		idea = {
-			"LFMoth",
-		},
-		art = {
-			"LFMoth",
-		},
-		code = {
-			"LFMoth + somethingcom",
-		},
-	},
     atlas = "enhancements",
     pos = { x = 1, y = 0 },
     config = { mult = 1 },
@@ -36,17 +25,6 @@ SMODS.Enhancement {
 -- Shingle
 SMODS.Enhancement {
     key = 'shingle',
-    cry_credits = {
-		idea = {
-			"LFMoth",
-		},
-		art = {
-			"LFMoth",
-		},
-		code = {
-			"LFMoth",
-		},
-	},
     atlas = "enhancements",
     pos = { x = 2, y = 0 },
     config = { durability = 3 },
@@ -76,7 +54,7 @@ SMODS.Enhancement {
     key = 'stacked',
     atlas = "enhancements",
     pos = { x = 3, y = 0 },
-    config = { count = 5 },
+    config = { count = 3 },
     replace_base_card = false,
     no_rank = false,
     overrides_base_rank = false,
