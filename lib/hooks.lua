62c67fbb87bf93fd8bc3bd9a4332639c87e909eb
LFMoth <95366051+LFMoth@users.noreply.github.com>
2026-02-06 14:14:56 +0100

    variadic arguments thing

--- lib/hooks.lua
+++ lib/hooks.lua
@@ -1,14 +1,14 @@
 local catcardgetid = Card.get_id -- Code to make Card Cat count as a Jack
-function Card:get_id()
+function Card:get_id(...)
     if SMODS.has_enhancement(self, 'm_req_cat') then
         return 11
     end
-    return catcardgetid(self, from_boss)
+    return catcardgetid(self, ...)
 end
 local catcardisface = Card.is_face -- Code to make Card Cat count as a face card
-function Card:is_face(from_boss)
+function Card:is_face(...)
     if SMODS.has_enhancement(self, 'm_req_cat') then
         return true
     end
-    return catcardisface(self, from_boss)
+    return catcardisface(self, ...)
 end
